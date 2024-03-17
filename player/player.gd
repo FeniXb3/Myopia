5aa8a7c78cf61995901681c6ad0f4fe9d2adf45f
Konrad Gadzina <konrad@makegames.today>
2024-03-17 23:22:31 +0100

    Handle body movement

--- player/player.gd
+++ player/player.gd
@@ -8,17 +8,23 @@ var current_direction := Vector2.RIGHT
 var next_direction := Vector2.RIGHT
 var move_tween : Tween
 @onready var head = %Head
+@onready var body_container = %BodyContainer
+var body_parts : Array = Array()
 
 @export var grid_size := Vector2(1920, 1080)
 @onready var area_2d = %Area2D
-@onready var collected_audio_steam_player = $CollectedAudioSteamPlayer
+@onready var collected_audio_steam_player = %CollectedAudioSteamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player_position.value = position
+	player_position.value = head.position
 	delay.value_changed.connect(_on_delay_changed)
 	SignalBus.collectible_entered.connect(_on_collectible_entered)
 	setup_move_tween()
+	for part in body_container.get_children():
+		body_parts.append(part)
+		
+	pass
 	
 	
 func setup_move_tween() -> void:
@@ -27,7 +33,12 @@ func setup_move_tween() -> void:
 		
 	if tween_movement.value:
 		move_tween = create_tween()
-		move_tween.tween_property(self, "position", player_position.value, delay.value).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
+		move_tween.tween_property(head, "position", player_position.value, delay.value).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
+		#move_tween.set_parallel(true)
+		for i in body_parts.size():
+			var target : Node2D = body_parts[i-1] if i > 0 else head
+			move_tween.parallel().tween_property(body_parts[i], "position", target.position, delay.value).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
+			
 		move_tween.tween_callback(move)
 	else:
 		move_tween = create_tween().set_loops()
@@ -49,13 +60,13 @@ func move() -> void:
 	if next_direction != current_direction and  current_direction.dot(next_direction) != -1:
 		var angle_radians := current_direction.angle_to(next_direction)
 		current_direction = next_direction
-		rotate(angle_radians)
+		head.rotate(angle_radians)
 	
-	var tmp_position = position;
+	var tmp_position = head.position;
 	
 	tmp_position += current_direction * step.value
-	tmp_position.x = fposmod(tmp_position.x, grid_size.x)
-	tmp_position.y = fposmod(tmp_position.y, grid_size.y)
+	#tmp_position.x = fposmod(tmp_position.x, grid_size.x)
+	#tmp_position.y = fposmod(tmp_position.y, grid_size.y)
 	
 	tmp_position = tmp_position.snapped(Vector2(step.value, step.value))
 	player_position.value = tmp_position
@@ -63,7 +74,11 @@ func move() -> void:
 	if tween_movement.value:
 		setup_move_tween()
 	else:
-		position = player_position.value
+		var last_part : Sprite2D = body_parts.pop_back()
+		last_part.position = head.position
+		body_parts.insert(0, last_part)
+		head.position = player_position.value
+		#position = player_position.value
 
 func _on_delay_changed(value : float) -> void:
 	if is_node_ready():
